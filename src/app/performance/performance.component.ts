cc1fcffe58b25e6a3a778df0aedfc1d773525468
Alexander van der Werff <alexander.vanderwerff@gmail.com>
2019-01-24 21:48:20 +0100

    app performance: improve page by css

--- src/app/performance/performance.component.ts
+++ src/app/performance/performance.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 import { CommunicationService } from '../communication.service';
-
+import { ChartFactory,  ChartMapEntry } from '../../app/chart.factory';
 
 
 @Component({
@@ -9,9 +9,24 @@ import { CommunicationService } from '../communication.service';
   styleUrls: ['./performance.component.css']
 })
 export class PerformanceComponent implements OnInit {
-  constructor(private communicationService: CommunicationService) { }
+  MIN_LATENCY = 40;
+  MAX_LATENCY = 1000;
+
+  private chartFactory = new ChartFactory();
+
+  @ViewChild('performanceGraph') transactionLatencyChart: ElementRef;
+  private charts = [];
+
+  constructor(private communicationService: CommunicationService) {
+    communicationService.stateChange.subscribe((newState) => {
+      this.chartFactory.renderCharts(this.charts, newState.states);
+    });
+  }
 
   ngOnInit() {
+    const ctxTransactionLatTop = this.transactionLatencyChart.nativeElement.getContext('2d');
+    const chartTransactionLatTop = this.chartFactory.getDefaultLineChart(ctxTransactionLatTop);
+    this.charts = [new ChartMapEntry('NoDBChart', chartTransactionLatTop, (state) => state.totaldbsize)];
   }
 
 
